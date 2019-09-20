c8f82893bbf5c7acee84887eb42ca4fcf8f5b01c
JGzuz <elec.jes.gomez@gmail.com>
2019-09-20 13:40:38 -0500

    node_arduino_serial_ok

--- server/index.js
+++ server/index.js
@@ -1,3 +1,11 @@
-//hola
-//gola como estas
-//recomentado
+const SerialPort = require("serialport")//importar modulo para comunicacion serial
+const mySerial = new SerialPort("COM18", {baudRate: 115200})//configuracion puerto y velocidad de transmision
+
+//evento comunicacion serial arbierta
+mySerial.on("open", function() {
+    console.log("COMUNICACION SERIAL ABIERTA")
+})
+
+mySerial.on("data", function (data) {
+    console.log(data.toString())
+})
