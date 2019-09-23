fac60e6b0c78d851cb1debeaac27170b44f3c0f8
JGzuz <elec.jes.gomez@gmail.com>
2019-09-23 11:54:54 -0500

    arduino_node_sim800LSimulado_OK

--- arduino_node_pic/arduino_node_pic.ino
+++ arduino_node_pic/arduino_node_pic.ino
@@ -1,25 +1,19 @@
 #include <SoftwareSerial.h>//libreria para implementar mas de un puerto serial
+SoftwareSerial serieNode(8,9);//configuracion puerto serie virtual (RX, TX)
 
-SoftwareSerial serieVirtual(8,9);//pines de transmision puerto virtual (RX, TX)
-int c = 0;
-String datosPic;//variable para almacenar datos de pic
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(115200);//velocidad de transmision arduino-pc
-  serieVirtual.begin(115200);//velocidad de transmision arduino-pic
+  Serial.begin(115200);while(!Serial){;}//configuracion velocidad de transmision
+  //arduino-pc, espera hasta que la comunicacion se haya establecido
+  Serial.println("Comunicacion arduino-pc establecida");
+
+  serieNode.begin(115200);//velocidad de transmison puerto virtual serie
+  serieNode.println("Puerto virtual inicializado");
   
 }
 
 void loop() {
-  // envia tres numeros por el puerto serie:
-  for(int i=0; i<=2; i++){
-  Serial.println(c++, DEC);
-  delay(1000);
-  }
-
-  //escucha si hay datos en el puerto serial virtual
-  if(serieVirtual.available()){
-      datosPic=serieVirtual.readString();//almacena datos provenientes del pic
+  if(serieNode.available()>0){
+      Serial.print((char)serieNode.read());
     }
 }
---
+++ arduino_node_pic/simula_sim800L/simula_sim800L.ino
@@ -0,0 +1,23 @@
+#include <SoftwareSerial.h>//libreria para configurar un puerto virtual serie
+SoftwareSerial serie_sim(8,9); //configuracion puerto virtual, (RX, TX)
+int x = 0;
+int pushBoton1 = 12;//variable aux para usar pin 12 como entrada
+
+void setup() {
+  serie_sim.begin(115200);
+  pinMode(pushBoton1, INPUT); //configura pin 12 como entrada
+}
+
+void loop() {
+  if((digitalRead(pushBoton1))==1){
+      switch(x){
+        case 0: serie_sim.println("0123456789");break;
+        case 1: serie_sim.println("1111111111");break;
+        case 2: serie_sim.println("2222222222");break;
+        case 3: serie_sim.println("3333333333");break;
+        case 4: serie_sim.println("4444444444");break;
+          
+      }x++; delay(1000);if(x==4){x=0;}
+    }
+
+}
