ae96d9f2355d9628b4b807502b0e95725b169fd0
JGzuz <elec.jes.gomez@gmail.com>
2019-10-05 12:31:21 -0500

    trasmisionn dual

--- arduino_node_pic/twodevices/nodeA/nodeA.ino
+++ arduino_node_pic/twodevices/nodeA/nodeA.ino
@@ -1,22 +1,29 @@
-
+#include <SoftwareSerial.h>
+SoftwareSerial Serial_sim(8,9);
 
 const int led11 = 11;
+const int led6 = 6;
 String megaDato;
 
 void setup() {
     Serial.begin(115200);
+    Serial_sim.begin(115200);
     delay(50);
+    Serial.print("COMUNICACION ARD-PC, ARD-SIM INICIALIZADAS");
     pinMode(led11, OUTPUT);
+    pinMode(led6, OUTPUT);
+    Serial_sim.print("1020120");
   } 
 
 void loop() {
     if(Serial.available()){
-        String data = Serial.readStringUntil('\n');
+        String data = (String)Serial.readStringUntil('\n');
         megaDato = data;
         Serial.print(data);
+        Serial.print(megaDato);
       }
 
-    if(megaDato=='A'){digitalWrite(led11, HIGH);}
-    else if (megaDato=='B') digitalWrite(led11, LOW);
+    if(megaDato == "A"){digitalWrite(led11, HIGH);Serial_sim.print("1020120");Serial_sim.print(megaDato);Serial_sim.print("");}
+    if(megaDato == "COCO"){digitalWrite(led6, HIGH);}
       
   }
--- arduino_node_pic/twodevices/sim800/sim800.ino
+++ arduino_node_pic/twodevices/sim800/sim800.ino
@@ -1,9 +1,26 @@
+#include <SoftwareSerial.h>
+SoftwareSerial Serial_sim(8,9);
+
+const int led10 = 10;
+const int led6 = 6;
+String megaDato;
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial_sim.begin(115200);
+  Serial.print("COMUNICACION ARD-PC, ARD-SIM INICIALIZADAS");
+  pinMode(led10, OUTPUT);
+  pinMode(led6, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial_sim.available()){
+      Serial.print("Reciviendo datos por el puerto serial ");
+      String data = (String)Serial_sim.readStringUntil('\n');
+      megaDato = data;
+      Serial.print(data);
+      Serial.print(megaDato);
+    }
 }
