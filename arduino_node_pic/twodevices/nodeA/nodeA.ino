03c3dc5d670c095f05c0e98766c13e2e6da38ef5
JGzuz <elec.jes.gomez@gmail.com>
2019-10-05 14:22:46 -0500

    cguodld

--- arduino_node_pic/twodevices/nodeA/nodeA.ino
+++ arduino_node_pic/twodevices/nodeA/nodeA.ino
@@ -4,6 +4,7 @@ SoftwareSerial Serial_sim(8,9);
 const int led11 = 11;
 const int led6 = 6;
 String megaDato;
+int long datoNumerico;
 
 void setup() {
     Serial.begin(115200);
@@ -19,11 +20,31 @@ void loop() {
     if(Serial.available()){
         String data = (String)Serial.readStringUntil('\n');
         megaDato = data;
-        Serial.print(data);
+        datoNumerico = (int)megaDato;
+        //Serial.print(data);
         Serial.print(megaDato);
       }
 
-    if(megaDato == "A"){digitalWrite(led11, HIGH);Serial_sim.print("1020120");Serial_sim.print(megaDato);Serial_sim.print("");}
-    if(megaDato == "COCO"){digitalWrite(led6, HIGH);}
+    if(megaDato == "A"){
+      digitalWrite(led11, HIGH);
+      digitalWrite(led6, LOW);
+      Serial_sim.print("222222");
+      //Serial_sim.print(megaDato);
+      Serial_sim.print("");
+      megaDato = "";
+      }
+      
+    if(megaDato == "COCO"){
+      digitalWrite(led6, HIGH);
+      digitalWrite(led11, LOW);
+      Serial_sim.print("333333");
+      //Serial_sim.print(megaDato);
+      Serial_sim.print("");
+      megaDato = "";
+      }
+      
+     switch (datoNumerico) {
+      case 'COCO': break;
+      }
       
   }
--- arduino_node_pic/twodevices/sim800/sim800.ino
+++ arduino_node_pic/twodevices/sim800/sim800.ino
@@ -20,7 +20,15 @@ void loop() {
       Serial.print("Reciviendo datos por el puerto serial ");
       String data = (String)Serial_sim.readStringUntil('\n');
       megaDato = data;
-      Serial.print(data);
+      //Serial.print(data);
       Serial.print(megaDato);
+    }
+  if(megaDato == "333333"){
+      digitalWrite(led10, HIGH);
+      digitalWrite(led6, LOW);
+    }
+  if(megaDato == "222222"){
+      digitalWrite(led10, LOW);
+      digitalWrite(led6, HIGH);
     }
 }
