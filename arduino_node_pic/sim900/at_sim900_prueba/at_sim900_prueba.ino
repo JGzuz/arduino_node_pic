b38dbc17ac0f84821cb3ed47ef2d1d98ae133181
JGzuz <elec.jes.gomez@gmail.com>
2019-10-30 15:56:58 -0600

    comunicacion ok con sim900

--- arduino_node_pic/sim900/at_sim900_prueba/at_sim900_prueba.ino
+++ arduino_node_pic/sim900/at_sim900_prueba/at_sim900_prueba.ino
@@ -6,10 +6,16 @@ void setup() {
   Serial.begin(19200);
   sim900.begin(19200);
 
+  sim900.print("A");
+  delay(500);
   sim900.print("AT\r");
   delay(200);
-  sim900.print("AT+CMFG=1\r");
+  sim900.print("AT\r");
   delay(200);
+  sim900.println("AT+CMGF=1\r");
+  delay(100);
+  sim900.println("AT+CNMI=1,2,0,0,0\r");
+  delay(100);
   
 }
 
